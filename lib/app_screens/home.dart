3ce596ae2d5bd91fed55728269d2a577f9b28f73
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-11-13 16:55:00 +0600

    Wed, Nov 13, 2019  4:54:59 PM

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -6,55 +6,126 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child:Container(
-        alignment: Alignment.center,
-        color: Colors.purpleAccent,
-        child:Row(
-          children: <Widget>[
-            Expanded(child:
-            Text(
-              "Dhaka", textDirection: TextDirection.ltr,
-              style: TextStyle(
-                decoration: TextDecoration.none,
-                fontFamily: 'Raleway',
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w300,
-                fontSize: 75.0,
+      child: Container(
+          alignment: Alignment.center,
+          color: Colors.purpleAccent,
+          padding: EdgeInsets.only(left: 15.0, top: 40.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      "Bangladesh Airlines",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Raleway',
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontSize: 35.0,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Dhaka to Chiangmai",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Raleway',
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            ),
-            Expanded(child:
-            Text(
-              "Shanhai", textDirection: TextDirection.ltr,
-              style: TextStyle(
-                decoration: TextDecoration.none,
-                fontFamily: 'Raleway',
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w300,
-                fontSize: 75.0,
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      "US Airlines",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Raleway',
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontSize: 35.0,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "NY to Boston",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontFamily: 'Raleway',
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            ),
-            Expanded(child:
-            Text(
-              "Kualalampur", textDirection: TextDirection.ltr,
-              style: TextStyle(
-                decoration: TextDecoration.none,
-                fontFamily: 'Raleway',
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w300,
-                fontSize: 75.0,
-              ),
-            ),
-            ),
+              FlightImageAsset(),
+              FlightBookButton(),
+            ],
+          )),
+    );
+  }
+}
 
+class FlightImageAsset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage("images/flight.png");
+    Image image = Image(image: assetImage, width: 200.0, height: 200.0);
 
+    return Container(child: image);
+  }
+}
 
-          ],
-
+class FlightBookButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150.0,
+      height: 50.0,
+      margin: EdgeInsets.only(top: 30.0),
+      child: RaisedButton(
+        color: Colors.deepOrange,
+        child: Center(
+          child: Text(
+            "Book your flight",
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.white,
+              fontFamily: "Raleway",
+              fontWeight: FontWeight.w700,
+            ),
+          ),
         ),
+        elevation: 6.0,
+        onPressed: () => bookFlight(context),
       ),
     );
+  }
 
+  void bookFlight(BuildContext context) {
+    var alertDialog = AlertDialog(
+        title: Text("Booked successfully"),
+        content: Text("Have a pleasant flight"));
+
+    showDialog(
+        context: context, builder: (BuildContext context) => alertDialog);
   }
 }
